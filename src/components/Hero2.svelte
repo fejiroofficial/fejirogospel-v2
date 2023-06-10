c9a1542400d38ffa7cfe646c99bd0c6964af31e9
Fejiro Gospel <fejiro.gospel@gmail.com>
2023-06-10 09:42:16 +0100

    feat: a new hero component

---
+++ src/components/Hero2.svelte
@@ -0,0 +1,26 @@
+<script>
+	// your script goes here
+</script>
+
+<section class="container mx-auto grid mt-8">
+	<div class="web-intro">
+		<h1>I am <br class="block md:hidden" /> Fejiro <br class="hidden md:block" /> Gospel</h1>
+	</div>
+	<p>Lead front-end developer based in the united kingdom</p>
+</section>
+<hr />
+
+<style lang="postcss">
+	.web-intro {
+		@apply pb-10 font-poppins overflow-x-scroll md:py-10 md:overflow-visible lg:mt-[120px];
+	}
+	.web-intro h1 {
+		@apply font-medium whitespace-nowrap text-[120px] leading-[100px] md:text-[120px] md:leading-[100px] lg:text-[150px] lg:leading-[123px] xl:text-[203px] xl:leading-[173px];
+	}
+	.container p {
+		@apply mt-[20px] font-light text-left text-[18px] leading-[28px] md:mt-[50px] md:text-[20px] md:leading-[32px] lg:text-[24px] lg:leading-[48px]  xl:mt-[50px] xl:text-[32px] xl:leading-[48px];
+	}
+	hr {
+		@apply mt-10 lg:mt-20;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Hero from "@components/Hero.svelte";
+	import Hero from "@components/Hero2.svelte";
 	import WorkSummary from "@components/WorkSummary.svelte";
 </script>
 
