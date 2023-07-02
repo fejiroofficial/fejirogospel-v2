9ed8be43f1d6b569517da41632aff1919edda3e7
Fejiro Gospel <fejiro.gospel@gmail.com>
2023-07-02 18:03:42 +0100

    feat: add about me

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,18 +1,44 @@
 <section class="about">
 	<h1>About Me</h1>
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua. Nunc mattis enim ut tellus elementum sagittis vitae et.
-		Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Et tortor at risus viverra
-		adipiscing at in tellus integer. Integer vitae justo eget magna. Elit duis tristique
-		sollicitudin nibh sit amet commodo. Nunc scelerisque viverra mauris in aliquam sem fringilla.
+		Once upon a time, in the realm of technology, there lived a skilled front-end developer named
+		Fejiro. With a remarkable career spanning over 5 years in web development, Fejiro had become a
+		valuable asset at LiveLink Technology. What made Fejiro's journey even more remarkable was the
+		decision to fully embrace frontend development in 2020 after starting as a full-stack developer.
+		Since then, Fejiro had found true passion and fulfillment in crafting captivating user
+		interfaces and seamless web experiences.
 	</p>
 	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua. Nunc mattis enim ut tellus elementum sagittis vitae et.
-		Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Et tortor at risus viverra
-		adipiscing at in tellus integer. Integer vitae justo eget magna. Elit duis tristique
-		sollicitudin nibh sit amet commodo. Nunc scelerisque viverra mauris in aliquam sem fringilla.
+		Throughout the years, Fejiro had taken on numerous leadership roles, guiding and inspiring teams
+		with their expertise and dedication. Their unwavering passion for all things web had become
+		infectious, fueling the creativity and productivity of those around them. Fejiro's commitment to
+		excellence and their ability to bring out the best in others made them a highly respected figure
+		in the realm of frontend development.
+	</p>
+	<p>
+		Outside the realm of coding and technology, Fejiro cherished moments spent with their beloved
+		family. At the heart of it all was Eliorah, their enchanting daughter, and Lilian, their
+		wonderful wife. Together, they created a warm and nurturing haven where love and happiness
+		thrived.
+	</p>
+	<p>
+		In the realm of hobbies and personal interests, Fejiro's spirit blossomed. They found solace in
+		the pages of books, exploring various worlds and ideas with each turn. Running became a metaphor
+		for their determination and drive, pushing boundaries and overcoming obstacles, both in life and
+		in their projects. Photography and art allowed Fejiro to capture and express the beauty they saw
+		in the world, transforming ordinary moments into extraordinary works of visual delight.
+	</p>
+	<p>
+		In this vast digital landscape, Fejiro's voice could be heard echoing on Twitter, where they
+		eagerly shared their insights, knowledge, and experiences with fellow developers and web
+		enthusiasts. You could easily find Fejiro in the Twitterverse, ready to connect and engage with
+		the vibrant web development community, always embracing the opportunity to learn and grow
+		alongside others.
+	</p>
+	<p>
+		And so, the story of Fejiro, the passionate frontend developer, continued to unfold, leaving an
+		indelible mark on the web and inspiring others to pursue their own dreams in the ever-evolving
+		realm of technology.
 	</p>
 </section>
 
@@ -20,10 +46,10 @@
 	.about {
 		@apply container mx-auto py-20 font-poppins;
 	}
-  .about h1 {
-    @apply text-2xl mb-9;
-  }
-  .about p {
-    @apply text-lg mb-7 md:text-2xl md:mb-9;
-  }
+	.about h1 {
+		@apply text-2xl mb-9;
+	}
+	.about p {
+		@apply text-lg mb-7 font-light md:text-2xl md:mb-9;
+	}
 </style>
