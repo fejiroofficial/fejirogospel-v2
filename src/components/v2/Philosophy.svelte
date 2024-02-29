0f32947e5411171454d90634fa72a8fe21895eb2
Fejiro Gospel <fejiro.gospel@gmail.com>
2024-02-29 20:34:13 +0000

    update: philosophy component

--- src/components/v2/Philosophy.svelte
+++ src/components/v2/Philosophy.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <section class="bg-pure-white pt-0 py-20 lg:pt-20">
-  <div class="container mx-auto grid grid-cols-12 ">
+  <div class="container mx-auto grid grid-cols-12 gap-6">
     <img class="col-span-12 mb-6 lg:col-span-6 lg:mb-0" src={displayImage} alt="">
     <div class="col-span-12 lg:col-span-6 pl-6 flex flex-col justify-center text-center lg:text-left">
       <h2 class="text-blue">My Philosophy</h2>
@@ -15,12 +15,12 @@
 
 <style lang="postcss">
   h2 {
-    @apply font-semibold font-poppins text-[28px] mb-2;
+    @apply font-semibold font-poppins text-[18px] mb-2 lg:text-[28px];
   }
   p {
-    @apply font-inter font-normal text-[38px] leading-[120%];
+    @apply font-inter font-normal text-[24px] leading-[120%] lg:text-[38px];
   }
   img {
-    @apply h-[40rem] flex justify-self-center rounded-3xl;
+    @apply h-[27rem] lg:h-[40rem] flex justify-self-center rounded-3xl;
   }
 </style>
