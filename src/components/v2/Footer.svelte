d8c0be724da7d7c51772b4621b8ce6e5315d2580
Fejiro Gospel <fejiro.gospel@gmail.com>
2024-03-02 18:12:03 +0000

    feat: footer section

---
+++ src/components/v2/Footer.svelte
@@ -0,0 +1,33 @@
+<script>
+	// your script goes here
+</script>
+
+<footer>
+	<div class="card">
+		<div class="footer-pane">
+			<div>
+				<a href="/v2">
+					<div class="logo">Fejiro Gospel</div>
+				</a>
+        <p class="text-lg mt-4">Code, Create, Coach</p>
+			</div>
+      
+		</div>
+		<hr />
+		<div>
+			<p class="text-xs">© 2024 CodeStream</p>
+		</div>
+	</div>
+</footer>
+
+<style lang="postcss">
+	footer {
+		@apply p-2 bg-white text-white font-inter flex justify-center;
+	}
+	.card {
+		@apply bg-black rounded-3xl px-[40px] py-[32px] max-w-[1500px] flex-auto;
+	}
+	hr {
+		@apply my-8;
+	}
+</style>
--- src/routes/(latest)/coaching/+page.svelte
+++ src/routes/(latest)/coaching/+page.svelte
@@ -1,7 +1,8 @@
 <script>
-	import Coaching from '../../../components/v2/Coaching.svelte';
+	import Footer from '@components/v2/Footer.svelte';
+	import Coaching from '@components/v2/Coaching.svelte';
 	import Guide from '@components/v2/Guide.svelte';
-	import Philosophy from '../../../components/v2/Philosophy.svelte';
+	import Philosophy from '@components/v2/Philosophy.svelte';
 	import Hero from '@components/v2/Hero.svelte';
 	import Mentorship from '@components/v2/Mentorship.svelte';
 	import Cta from '@components/v2/Cta.svelte';
@@ -13,3 +14,4 @@
 <Coaching />
 <Mentorship />
 <Cta />
+<Footer />
