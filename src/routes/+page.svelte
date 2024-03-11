8dbb90a90d67e49c51abedde8855275f35205406
Fejiro Gospel <fejiro.gospel@gmail.com>
2024-03-11 09:17:10 +0000

    pointing homepage to newsletter

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+  import FormData from './form';
+
+
+</script>
+
+<svelte:head>
+	<script src="https://f.convertkit.com/ckjs/ck.5.js"></script>
+</svelte:head>
+
+<section class="container mx-auto pt-20 flex justify-center">{@html FormData}</section>
